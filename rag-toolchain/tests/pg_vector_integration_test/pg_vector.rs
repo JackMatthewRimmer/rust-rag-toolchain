5eb9ac81b47157952d6dba6ef7fecf308a29efb0
Jack Rimmer <rimbojnr@gmail.com>
2023-11-28 20:44:03 +0000

    refactor(#8): Fix runtime issues

--- rag-toolchain/tests/pg_vector_integration_test/pg_vector.rs
+++ rag-toolchain/tests/pg_vector_integration_test/pg_vector.rs
@@ -7,47 +7,54 @@ use sqlx::{Pool, Postgres};
 #[cfg(test)]
 mod pg_vector {
 
+    use sqlx::query;
+
     use super::*;
 
     #[tokio::test]
     async fn test_store_persists() {
+        const TABLE_NAME: &str = "test_db_1";
         std::env::set_var("POSTGRES_USER", "postgres");
         std::env::set_var("POSTGRES_PASSWORD", "postgres");
         std::env::set_var("POSTGRES_HOST", "localhost");
         std::env::set_var("POSTGRES_DATABASE", "pg_vector");
-        let pg_vector = PgVectorDB::new("test_db_1").await.unwrap();
+        let pg_vector = PgVectorDB::new(TABLE_NAME).await.unwrap();
         let _result = pg_vector.store(("test".into(), vec![1.0; 1536])).await.map_err(|_| panic!("panic"));
-        assert_row(&pg_vector.pool, 1, "test".into(), vec![1.0; 1536]).await;
+        assert_row(&pg_vector.pool, 1, "test".into(), vec![1.0; 1536], TABLE_NAME).await;
     }
 
     #[tokio::test]
     async fn test_batch_store_persists() {
+        const TABLE_NAME: &str = "test_db_2";
         std::env::set_var("POSTGRES_USER", "postgres");
         std::env::set_var("POSTGRES_PASSWORD", "postgres");
         std::env::set_var("POSTGRES_HOST", "localhost");
         std::env::set_var("POSTGRES_DATABASE", "pg_vector");
-        let pg_vector = PgVectorDB::new("test_db_2").await.unwrap();
+        let pg_vector = PgVectorDB::new(TABLE_NAME).await.unwrap();
         let input: Vec<(String, Vec<f32>)> = vec![
             ("test1".into(), vec![1.0; 1536]),
             ("test2".into(), vec![2.0; 1536]),
             ("test3".into(), vec![3.0; 1536]),
         ];
         let _result = pg_vector.store_batch(input).await.map_err(|_| panic!("panic"));
-        assert_row(&pg_vector.pool, 1, "test1".into(), vec![1.0; 1536]).await;
-        assert_row(&pg_vector.pool, 2, "test2".into(), vec![2.0; 1536]).await;
-        assert_row(&pg_vector.pool, 3, "test3".into(), vec![3.0; 1536]).await;
+        assert_row(&pg_vector.pool, 1, "test1".into(), vec![1.0; 1536], TABLE_NAME).await;
+        assert_row(&pg_vector.pool, 2, "test2".into(), vec![2.0; 1536], TABLE_NAME).await;
+        assert_row(&pg_vector.pool, 3, "test3".into(), vec![3.0; 1536], TABLE_NAME).await;
     }
 
-    async fn assert_row(pool: &Pool<Postgres>, id: i32, text: String, embeddings: Vec<f32>) -> () {
-        let row: RowData = query_row(pool, id).await;
+    async fn assert_row(pool: &Pool<Postgres>, id: i32, text: String, embeddings: Vec<f32>, table_name: &str) -> () {
+        let row: RowData = query_row(pool, id, table_name).await;
         assert_eq!(row.id, id);
         assert_eq!(row.content, text);
         assert_eq!(row.embedding, embeddings);
     }
 
-    async fn query_row(pool: &Pool<Postgres>, id: i32) -> RowData {
+    async fn query_row(pool: &Pool<Postgres>, id: i32, table_name: &str) -> RowData {
+
+        let query: String = format!("SELECT id, content, embedding FROM {} WHERE id = $1", table_name);
+
         let query: PgRow =
-            sqlx::query("SELECT id, content, embedding FROM embeddings WHERE id = $1")
+            sqlx::query(&query)
                 .bind(id)
                 .fetch_one(pool)
                 .await
