574e566d3abef3e843c0f955ae9374364ed30983
psyjr9 <psyjr9@nottingham.ac.uk>
2023-11-30 06:31:45 +0000

    15: Move examples inside examples dir

--- rag-toolchain/examples/pg_vector/main.rs
+++ rag-toolchain/examples/pg_vector/main.rs
@@ -2,13 +2,19 @@ use rag_toolchain::toolchain_embeddings::{
     embedding_models::OpenAIEmbeddingModel, openai_embeddings::OpenAIClient,
 };
 use rag_toolchain::toolchain_indexing::chunking::TokenChunker;
-use rag_toolchain::toolchain_indexing::stores::pg_vector::PgVector;
+use rag_toolchain::toolchain_indexing::stores::pg_vector::PgVectorDB;
 use rag_toolchain::toolchain_indexing::traits::EmbeddingStore;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    std::env::set_var("POSTGRES_USER", "postgres");
+    std::env::set_var("POSTGRES_PASSWORD", "postgres");
+    std::env::set_var("POSTGRES_HOST", "localhost");
+    std::env::set_var("POSTGRES_DATABASE", "pg_vector");
+
     // Given our text, we want to split it into chunks, Usually this would be done for larger texts
     // But has been done here for demonstration purposes
-    let text = std::fs::read_to_string("src/example_text.txt").unwrap();
+    let text = std::fs::read_to_string("examples/pg_vector/example_text.txt").unwrap();
     println!("Text: {}", text);
     let chunker = TokenChunker::new(
         std::num::NonZeroUsize::new(50).unwrap(),
@@ -20,18 +26,18 @@ fn main() {
     println!("Chunks: {:?}", chunks);
 
     // I would check your store initialized before sending of embeddings to openai
-    let store: PgVector = PgVector::new("embeddings").unwrap();
+    let store: PgVectorDB = PgVectorDB::new(
+        "embeddings",
+        OpenAIEmbeddingModel::TextEmbeddingAda002.into(),
+    )
+    .await
+    .unwrap();
 
     // Create a new client and generate the embeddings for the chunks
     let client: OpenAIClient = OpenAIClient::new().unwrap();
     let embeddings: Vec<(String, Vec<f32>)> = client.generate_embeddings(chunks).unwrap();
     println!("Embeddings: {:?}", embeddings);
 
-    // This will happen automatically in the future
-    store.create_table().unwrap();
-
     // Insert the embeddings into the store this will be a better interface in the future
-    for item in embeddings {
-        store.store(item).unwrap();
-    }
+    store.store_batch(embeddings).await.unwrap();
 }
