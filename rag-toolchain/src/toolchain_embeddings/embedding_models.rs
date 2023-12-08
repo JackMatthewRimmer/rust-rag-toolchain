2a61b4c151f4e959e1913d55f49ebac05048838c
psyjr9 <psyjr9@nottingham.ac.uk>
2023-12-08 08:16:32 +0000

    22: Add traits for embedding clients

--- rag-toolchain/src/toolchain_embeddings/embedding_models.rs
+++ rag-toolchain/src/toolchain_embeddings/embedding_models.rs
@@ -1,5 +1,27 @@
+use crate::toolchain_indexing::types::{Chunk, Chunks, Embedding};
+use async_trait::async_trait;
+use std::error::Error;
 use tiktoken_rs::tokenizer::Tokenizer;
 use tiktoken_rs::CoreBPE;
+
+/// # AsyncEmbeddingClient
+/// Trait for any client that generates embeddings asynchronously
+/// such as OpenAI
+#[async_trait]
+pub trait AsyncEmbeddingClient {
+    type ErrorType: Error;
+    async fn generate_embedding(text: Chunk) -> Result<(Chunk, Embedding), Self::ErrorType>;
+    async fn generate_embeddings(text: Chunks) -> Result<Vec<(Chunk, Embedding)>, Self::ErrorType>;
+}
+
+/// # EmbeddingClient
+/// Trait for any client that generates embeddings synchronously
+/// can be used for any local embedding model
+pub trait EmbeddingClient {
+    type ErrorType: Error;
+    fn generate_embedding(text: Chunk) -> Result<(Chunk, Embedding), Self::ErrorType>;
+    fn generate_embeddings(text: Chunks) -> Result<Vec<(Chunk, Embedding)>, Self::ErrorType>;
+}
 
 // ---------------------- Embedding Models ----------------------
 /// # HasMetadata
