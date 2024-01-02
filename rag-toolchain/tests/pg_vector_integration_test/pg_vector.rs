ccb571db826733e34e181b0793895b079b070427
JackMatthewRimmer <jackrimmerwork@outlook.com>
2024-01-02 20:13:15 +0000

    33: fix the issue

--- rag-toolchain/tests/pg_vector_integration_test/pg_vector.rs
+++ rag-toolchain/tests/pg_vector_integration_test/pg_vector.rs
@@ -80,7 +80,7 @@ mod pg_vector {
             .await
             .unwrap();
         let input: Vec<(Chunk, Embedding)> = read_test_data();
-        let data_to_store : Vec<(Chunk, Embedding)>= input[0..1].to_vec();
+        let data_to_store : Vec<(Chunk, Embedding)> = input[0..2].to_vec();
         let _result = pg_vector
             .store_batch(data_to_store.clone())
             .await
