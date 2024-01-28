5aa127fb9fa4eea759bc6d3850e1d09d5e50e1d6
JackMatthewRimmer <jackrimmerwork@outlook.com>
2024-01-28 11:23:18 +0000

    34: Start modelling completion response

--- rag-toolchain/src/clients/model/chat_completions.rs
+++ rag-toolchain/src/clients/model/chat_completions.rs
@@ -2,8 +2,27 @@ use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 use typed_builder::TypedBuilder;
 
-// --------------------------------------------------------------------------------
 /// See <https://platform.openai.com/docs/api-reference/embeddings/create>
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, TypedBuilder)]
+#[serde(rename_all = "snake_case")]
+pub struct ChatCompletionRequest {
+    pub model: OpenAIModel,
+    pub messages: Vec<ChatMessage>,
+    #[builder(default, setter(strip_option))]
+    #[serde(flatten)]
+    pub additional_config: Option<Map<String, Value>>,
+}
+
+pub struct ChatCompletionResponse {
+    pub id: String,
+    pub object: String,
+    pub created: u64,
+    pub model: OpenAIModel,
+    pub system_fingerprint: String,
+    pub choices: Vec<ChatCompletionChoices>,
+    pub usage: Usage,
+}
+
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum ChatMessageRole {
@@ -24,17 +43,27 @@ pub enum OpenAIModel {
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ChatMessage {
     pub role: ChatMessageRole,
-    pub message: String,
+    pub content: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, TypedBuilder)]
-#[serde(rename_all = "snake_case")]
-pub struct ChatCompletionRequest {
-    pub model: OpenAIModel,
-    pub messages: Vec<ChatMessage>,
-    #[builder(default, setter(strip_option))]
-    #[serde(flatten)]
-    pub additional_config: Option<Map<String, Value>>,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Usage {
+    pub prompt_tokens: usize,
+    pub completion_tokens: usize,
+    pub total_tokens: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct ChatCompletionChoices {
+    pub message: ChatCompletionChoice,
+    pub logprobs: Option<bool>,
+    pub finish_reason: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct ChatCompletionChoice {
+    pub message: ChatMessage,
+    pub index: usize,
 }
 
 pub mod request_model_tests {
@@ -52,15 +81,15 @@ pub mod request_model_tests {
             messages: vec![
                 ChatMessage {
                     role: ChatMessageRole::System,
-                    message: "Hello,howareyou?".into(),
+                    content: "Hello,howareyou?".into(),
                 },
                 ChatMessage {
                     role: ChatMessageRole::User,
-                    message: "I'mdoinggreat.Howaboutyou?".into(),
+                    content: "I'mdoinggreat.Howaboutyou?".into(),
                 },
                 ChatMessage {
                     role: ChatMessageRole::System,
-                    message: "I'mdoingwell.I'mgladtohearyou'redoingwell.".into(),
+                    content: "I'mdoingwell.I'mgladtohearyou'redoingwell.".into(),
                 },
             ],
             additional_config: Some(additional_config)
