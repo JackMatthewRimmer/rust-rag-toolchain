55aa4c8265689c0250de79342b5d6d6ec8e1c14b
JackMatthewRimmer <jackrimmerwork@outlook.com>
2024-03-01 19:58:09 +0000

    54: Start to remove async trait

--- rag-toolchain/src/stores/traits.rs
+++ rag-toolchain/src/stores/traits.rs
@@ -1,18 +1,17 @@
 use crate::common::{Chunk, Embedding};
-use async_trait::async_trait;
+use std::future::Future;
 use std::error::Error;
 
 /// # EmbeddingStore
 /// Trait for struct that allows embeddings to be wrote to an
 /// external persistent store. Mainly a vector database.
-#[async_trait]
 pub trait EmbeddingStore {
     // The custom error type for the store
     type ErrorType: Error;
     /// Takes an embedding and writes it to an external source
-    async fn store(&self, embedding: (Chunk, Embedding)) -> Result<(), Self::ErrorType>;
-    async fn store_batch(&self, embeddings: Vec<(Chunk, Embedding)>)
-        -> Result<(), Self::ErrorType>;
+    fn store(&self, embedding: (Chunk, Embedding)) -> impl Future<Output = Result<(), Self::ErrorType>> + Send;
+    fn store_batch(&self, embeddings: Vec<(Chunk, Embedding)>)
+        -> impl Future<Output = Result<(), Self::ErrorType>> + Send;
 }
 
 #[cfg(test)]
@@ -20,7 +19,6 @@ use mockall::*;
 #[cfg(test)]
 mock! {
     pub EmbeddingStore {}
-    #[async_trait]
     impl EmbeddingStore for EmbeddingStore {
         type ErrorType = std::io::Error;
         async fn store(&self, embedding: (Chunk, Embedding)) -> Result<(), <Self as EmbeddingStore>::ErrorType>;
