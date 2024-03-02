d8c8747862c84a1899ebcf0f6787b78559a58de5
JackMatthewRimmer <jackrimmerwork@outlook.com>
2024-03-02 09:11:51 +0000

    59: Add new openAI embedding models

--- rag-toolchain/examples/pg_vector/main.rs
+++ rag-toolchain/examples/pg_vector/main.rs
@@ -5,6 +5,9 @@ use rag_toolchain::stores::{EmbeddingStore, PostgresVectorStore};
 
 #[tokio::main]
 async fn main() {
+
+    const EMBEDDING_MODEL: OpenAIEmbeddingModel = OpenAIEmbeddingModel::TextEmbedding3Small;
+
     // We read in the text from a file
     let text = std::fs::read_to_string("examples/pg_vector/example_text.txt").unwrap();
     println!("Text: {}", text);
@@ -12,7 +15,7 @@ async fn main() {
     let chunker = TokenChunker::try_new(
         std::num::NonZeroUsize::new(50).unwrap(),
         25,
-        OpenAIEmbeddingModel::TextEmbeddingAda002,
+        EMBEDDING_MODEL,
     )
     .unwrap();
     let chunks: Chunks = chunker.generate_chunks(&text).unwrap();
@@ -20,15 +23,14 @@ async fn main() {
 
     // I would check your store initialized before sending of embeddings to openai
     let store: PostgresVectorStore =
-        PostgresVectorStore::try_new("embeddings", OpenAIEmbeddingModel::TextEmbeddingAda002)
+        PostgresVectorStore::try_new("embeddings", EMBEDDING_MODEL)
             .await
             .unwrap();
 
     // Create a new client and generate the embeddings for the chunks
     let client: OpenAIEmbeddingClient =
-        OpenAIEmbeddingClient::try_new(OpenAIEmbeddingModel::TextEmbeddingAda002).unwrap();
+        OpenAIEmbeddingClient::try_new(EMBEDDING_MODEL).unwrap();
     let embeddings: Vec<(Chunk, Embedding)> = client.generate_embeddings(chunks).await.unwrap();
-    println!("Embeddings: {:?}", embeddings);
 
     // Insert the embeddings into the store
     store.store_batch(embeddings).await.unwrap();
