f002aa84f0415af4f8bd10690f49b530f6575e7c
Jack Rimmer <jackrimmer@Jacks-MacBook-Pro.local>
2024-05-13 21:23:24 +0100

    99: Add openai feature

--- rag-toolchain/src/clients/mod.rs
+++ rag-toolchain/src/clients/mod.rs
@@ -1,14 +1,22 @@
 /// # Clients
 /// This module will contain all of the client code for different services
 /// that can be used to interact with Gen AI models.
+#[cfg(feature = "openai")]
 mod open_ai;
+
 mod traits;
 mod types;
+
+#[cfg(feature = "openai")]
 pub use self::open_ai::open_ai_chat_completions::{
     CompletionStreamValue, OpenAIChatCompletionClient, OpenAICompletionStream,
 };
+
+#[cfg(feature = "openai")]
 pub use self::open_ai::open_ai_embeddings::OpenAIEmbeddingClient;
+#[cfg(feature = "openai")]
 pub use self::open_ai::{OpenAIError, OpenAIModel};
+
 pub use self::traits::{
     AsyncChatClient, AsyncEmbeddingClient, AsyncStreamedChatClient, ChatCompletionStream,
 };
