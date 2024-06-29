eb5178a4cb35f4b46b39a5d52af27e2068528dff
JackMatthewRimmer <rimbojnr@gmail.com>
2024-06-29 09:30:33 +0100

    128: Add most of the character chunker impl

--- rag-toolchain/src/chunkers/character_chunker.rs
+++ rag-toolchain/src/chunkers/character_chunker.rs
@@ -1,3 +1,8 @@
+use crate::chunkers::{Chunker, StreamedChunker};
+use crate::common::{Chunk, Chunks};
+use std::convert::Infallible;
+use std::error::Error;
+use std::fmt::Display;
 use std::num::NonZeroUsize;
 
 pub struct CharacterChunker {
@@ -24,4 +29,29 @@ impl CharacterChunker {
             chunk_overlap,
         }
     }
+}
+
+impl Chunker for CharacterChunker {
+    type ErrorType = Infallible;
+    fn generate_chunks(&self, raw_text: &str) -> Result<Chunks, Self::ErrorType> {
+        let mut chunks: Chunks = Vec::new();
+        let chunk_size: usize = self.chunk_size.into();
+
+        let mut i = 0;
+        while i < raw_text.len() {
+            let end = std::cmp::min(i + chunk_size, raw_text.len());
+            let chunk: Chunk = Chunk::new(&raw_text[i..end]);
+            chunks.push(chunk);
+            i += chunk_size - self.chunk_overlap;
+        }
+
+        Ok(chunks)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: will need test cases when the constructor becomes try_new.
 }
